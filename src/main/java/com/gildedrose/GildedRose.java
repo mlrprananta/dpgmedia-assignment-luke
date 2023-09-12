b04c5134adde15085e90f45d782de00451207107
mlrprananta <lukeprananta@gmail.com>
2023-09-12 02:01:26 +0200

    Second refactor pass

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,12 +1,5 @@
 package com.gildedrose;
 
-/**
- * Some constraints:
- *
- * <ul>
- *   <li>Item class and items property can not be altered
- * </ul>
- */
 class GildedRose {
   Item[] items;
 
@@ -14,36 +7,52 @@ class GildedRose {
     this.items = items;
   }
 
+  /**
+   * Some assumptions were made that should be clarified:
+   *
+   * <ul>
+   *   <li>Quality is never negative and never more than 50. Should items created outside of these
+   *       boundaries be corrected?
+   *   <li>Do the effects of conjuration apply on top of the effects of the item (i.e. do Conjured
+   *       Aged Brie items increase in quality twice)? Assumption: yes.
+   *   <li>Can there be more than one legendary item? Assumption: yes.
+   *   <li>Should legendary items be checked to have quality of 80?
+   * </ul>
+   */
   public void updateQuality() {
     for (Item item : items) {
       String name = item.name;
-      int degradation = -1;
-      int degradationFactor = 1;
-      if (item.sellIn <= 0) {
-        degradationFactor *= 2;
-      }
+
+      int qualityDelta = -1;
+      int deltaFactor = 1;
+
       if (name.startsWith("Conjured")) {
-        degradationFactor *= 2;
+        deltaFactor *= 2;
         name = item.name.substring("Conjured ".length());
       }
       if (name.equals("Sulfuras, Hand of Ragnaros")) continue;
+
+      item.sellIn -= 1;
+
+      if (item.sellIn < 0) {
+        deltaFactor *= 2;
+      }
       if (name.equals("Aged Brie")) {
-        degradation = 1;
+        qualityDelta = 1;
       }
       if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        degradation = 1;
-        if (item.sellIn < 11) {
-          degradation += 1;
+        qualityDelta = 1;
+        if (item.sellIn < 10) {
+          qualityDelta += 1;
         }
-        if (item.sellIn < 6) {
-          degradation += 1;
+        if (item.sellIn < 5) {
+          qualityDelta += 1;
         }
-        if (item.sellIn <= 0) {
-          degradation = -item.quality;
+        if (item.sellIn < 0) {
+          qualityDelta = -item.quality;
         }
       }
-      item.quality = Math.max(0, Math.min(item.quality + degradation * degradationFactor, 50));
-      item.sellIn -= 1;
+      item.quality = Math.max(0, Math.min(item.quality + qualityDelta * deltaFactor, 50));
     }
   }
 }
