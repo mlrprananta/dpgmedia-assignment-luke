cefb1d0884fc549670b68d6ca3c024ca21a70be0
mlrprananta <lukeprananta@gmail.com>
2023-09-12 17:23:51 +0200

    Third refactor pass

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,9 +15,8 @@ class GildedRose {
    *       boundaries be corrected? Assumptions:
    *       <ul>
    *         <li>Cannot decrease to negative or increase over 50 from valid quality.
-   *         <li>Decreasing the quality of currently negative will set the quality to
-   *             zero.
-   *         <li>Increasing the quality of currently over 50 will set the quality to 50.
+   *         <li>Decreasing the quality from currently negative will set the quality to zero.
+   *         <li>Increasing the quality from currently over 50 will set the quality to 50.
    *         <li>Increasing quality from negative will increase from 0.
    *         <li>Decreasing quality from over 50 will decrease from 50.
    *       </ul>
@@ -31,18 +30,14 @@ class GildedRose {
   public void updateQuality() {
     for (Item item : items) {
       String name = item.name;
-
       int qualityDelta = -1;
       int deltaFactor = 1;
-
       if (name.startsWith("Conjured")) {
         deltaFactor *= 2;
         name = item.name.substring("Conjured ".length());
       }
       if (name.equals("Sulfuras, Hand of Ragnaros")) continue;
-
       item.sellIn -= 1;
-
       if (item.sellIn < 0) {
         deltaFactor *= 2;
       }
@@ -50,24 +45,34 @@ class GildedRose {
         qualityDelta = 1;
       }
       if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        qualityDelta = 1;
-        if (item.sellIn < 10) {
-          qualityDelta += 1;
-        }
-        if (item.sellIn < 5) {
-          qualityDelta += 1;
-        }
-        if (item.sellIn < 0) {
-          qualityDelta = -item.quality;
-        }
+        qualityDelta = setBackstagePassesQualityDelta(item);
       }
-      if (item.quality < 0) {
-        item.quality = 0;
-      }
-      if (item.quality > 50) {
-        item.quality = 50;
-      }
+      correctInvalidQuality(item);
       item.quality = Math.max(0, Math.min(item.quality + qualityDelta * deltaFactor, 50));
     }
+  }
+
+  private static void correctInvalidQuality(Item item) {
+    if (item.quality < 0) {
+      item.quality = 0;
+    }
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
+  }
+
+  private static int setBackstagePassesQualityDelta(Item item) {
+    int qualityDelta;
+    qualityDelta = 1;
+    if (item.sellIn < 10) {
+      qualityDelta += 1;
+    }
+    if (item.sellIn < 5) {
+      qualityDelta += 1;
+    }
+    if (item.sellIn < 0) {
+      qualityDelta = -item.quality;
+    }
+    return qualityDelta;
   }
 }
